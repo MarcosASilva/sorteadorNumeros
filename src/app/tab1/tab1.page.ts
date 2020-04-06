1108544d99eb76d095b7b9d213e31683347c4c2c
Marcos Assis <marcos.assis@erplan.com.br>
2020-04-06 16:09:10 -0300

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  numInicial = 1;
+  numInicialLoteria = 1;
+  numFinal = 1;
+  numFinalLoteria = 60;
+  sorteado = false;
+  numSorteado = 0
+  modo = 'simples'
+  numLoteria = []
   constructor() {}
 
+
+  sortear = () => {
+    if(this.modo==='mega'||this.modo==='quina'){
+      return this.sortearLoteria();
+    }
+
+    let min = Math.ceil(this.numInicial);
+    
+    let max = Math.floor(this.numFinal);
+   
+    this.numSorteado =  Math.floor(Math.random() * (max - min)) + min;
+    this.sorteado = true
+    
+  }
+  sortearLoteria = () => {
+    this.numLoteria = []
+    let min = Math.ceil(this.numInicialLoteria );
+    let max = Math.floor(this.numFinalLoteria + 1);
+    let x = 0
+    if(this.modo==='mega'){
+       x = 60
+    }else{
+       x = 5
+    }
+
+    for(let i=0;i< x;i++){
+
+      let num =  Math.floor(Math.random() * (max - min)) + min;
+      while(this.numLoteria.includes(num)){
+        num =  Math.floor(Math.random() * (max - min)) + min;
+      }
+      this.numLoteria.push(num)
+    }
+    this.sorteado = true
+    this.numLoteria.sort((a,b) => {
+      return a - b;
+    })
+    console.log(this.numLoteria);
+    
+    
+  }
 }
